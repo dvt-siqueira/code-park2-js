a5e3a3ecce1bde1124dff9a2a8cd5b4877930c15
Davi Tavares <dvt.siqueira@gmail.com>
2024-04-01 16:01:02 -0300

    updade ul e ol

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ console.log('conectou');
 const h1 = document.getElementById("titulo");
 const alink = document.querySelector("a");
 const dataOl = document.querySelector("ol")
+const dataUl = document.querySelector("ul")
 
 console.log(alink);
 
 h1.innerText = "Titulo do H1";
 
 alink.innerHTML = "Link dinamico da Proz";
-dataOl.innerHTML="<li>Oi</li><li>Tchau</li><li>Bye Bye</li>"
+dataOl.innerHTML=
+        `<li>Oi</li>
+        <li>Tchau</li>
+        <li>Bye Bye</li>`;
+dataUl.innerHTML=
+        `<li>UL - Oi</li>
+        <li>UL - Tchau</li>
+        <li>UL - Bye Bye</li>`
